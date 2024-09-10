b7cc00fe3d5fa9734d5317caf87f45abda242b32
Cletus Kingdom <cletuskingdom@gmail.com>
2024-09-10 17:41:23 +0100

    Done many updates

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,80 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+    <q-page class="flex flex-center text-white">
+        <div class="row">
+            <q-input
+                v-model="data.name"
+                input-class="text-center text-h5 text-white"
+                filled 
+                color="teal"
+                placeholder="Placeholder"
+            />
+        </div>
+
+        <div class="row full-width item-center">
+            <div class="col text-center">
+                <q-btn
+                    @click="decreaseCounter"
+                    round 
+                    icon="remove"
+                    size="xl"
+                />
+            </div>
+
+            <div class="col text-center text-h2">
+                {{ data.counter }}
+            </div>
+
+            <div class="col text-center">
+                <q-btn
+                    @click="increaseCounter"
+                    round 
+                    icon="add"
+                    size="xl"
+                />
+            </div>
+        </div>
+
+        <div class="row">
+            <q-btn
+                @click="resetCounter"
+                icon="restart_alt"
+                round 
+                size="xl"
+            /> 
+        </div>
+    </q-page>
 </template>
 
+
+<style scoped>
+    .q-page{
+        max-width: 700px;
+        margin: 0 auto;
+    }
+</style>
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+    // imports start
+    import {reactive} from 'vue'
+    // imports end
+
+    // data start
+    const data = reactive({
+        counter: 0,
+        name: ''
+    })
+    // data end
+
+    // counter methods start
+    const increaseCounter = () => {
+        data.counter++
+    }
+
+    const decreaseCounter = () => {
+        if(data.counter > 0) data.counter--
+    }
+
+    const resetCounter = () => {
+        data.counter = 0
+    }
+    // counter methods end
 </script>
